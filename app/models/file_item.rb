00fd95f960294a7071057b1ed1681363587e8ce1
ryan_shadow <ryan_shadow@example.com>
2021-06-19 18:58:41 +0900

    validation修正

--- app/models/file_item.rb
+++ app/models/file_item.rb
@@ -1,6 +1,13 @@
 class FileItem < ApplicationRecord
   validates :name, presence: true, length: { maximum: 10 }
   validates :image_file, presence: true
+  validate :image_file, :size_validate
 
   has_one_attached :image_file
+
+  def size_validate
+    if image_file.blob.byte_size > 5.megabytes
+      errors.add(:image_file, "は5MB以内にしてください")
+    end
+  end
 end
